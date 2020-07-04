748f1f585619e2d2d4ee647b7dd7d2ea41e967bf
Yasmin <ysobarbosa@gmail.com>
2020-07-04 13:24:35 -0300

    final commit

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace Exemplorefit
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace Exemplorefit
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+           try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep: ");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine($"Consultando informações do CEP: {cepInformado}");
+
+                var adress = await cepClient.GetAddressAsync(cepInformado);
+                 
+                Console.WriteLine($"\nLogradouro: {adress.Logradouro}, \nBairro: {adress.Bairro}, \nCidade: {adress.Localidade}" +
+                    $"\nEstado:{adress.UF}");
+                Console.ReadKey();
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            } 
+        }
+    }
+}
